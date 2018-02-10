6f4ed4368ceb4f31656537c4615f82124dd08616
geoo89 <geoo89@gmail.com>
2018-02-10 23:18:07 +0100

    level data split off into separate files

---
+++ level/maporder.asm
@@ -0,0 +1,109 @@
+;MapOrder_Index:	
+	dc.b $4A	; 0
+	dc.b $4B	; 1
+	dc.b $2D	; 2
+	dc.b $2E	; 3
+	dc.b   1	; 4
+	dc.b   0	; 5
+	dc.b   2	; 6
+	dc.b  $E	; 7
+	dc.b $54	; 8
+	dc.b $56	; 9
+	dc.b $18	; A
+	dc.b $1D	; B
+	dc.b $3D	; C
+	dc.b $37	; D
+	dc.b $2C	; E
+	dc.b $2B	; F
+	dc.b $13	;10
+	dc.b $4C	;11
+	dc.b $49	;12
+	dc.b $14	;13
+	dc.b $16	;14
+	dc.b  $B	;15
+	dc.b  $C	;16
+	dc.b $3F	;17
+	dc.b $40	;18
+	dc.b $34	;19
+	dc.b $1C	;1A
+	dc.b $27	;1B
+	dc.b $21	;1C
+	dc.b  $D	;1D
+	dc.b $6C	;1E
+	dc.b $4F	;1F
+	dc.b $4D	;20
+	dc.b $19	;21
+	dc.b  $A	;22
+	dc.b $5C	;23
+	dc.b $25	;24
+	dc.b $26	;25
+	dc.b $35	;26
+	dc.b $3E	;27
+	dc.b $2F	;28
+	dc.b $31	;29
+	dc.b $74	;2A
+	dc.b $3B	;2B
+	dc.b $5A	;2C
+	dc.b $33	;2D
+	dc.b $3A	;2E
+	dc.b $57	;2F
+	dc.b $10	;30
+	dc.b $59	;31
+	dc.b $38	;32
+	dc.b   5	;33
+	dc.b $30	;34
+	dc.b $55	;35
+	dc.b $32	;36
+	dc.b  $F	;37
+	dc.b $48	;38
+	dc.b $47	;39
+	dc.b   3	;3A
+	dc.b $53	;3B
+	dc.b   4	;3C
+	dc.b $36	;3D
+	dc.b $15	;3E
+	dc.b $12	;3F
+	dc.b $22	;40
+	dc.b $41	;41
+	dc.b $17	;42
+	dc.b $4E	;43
+	dc.b $58	;44
+	dc.b $11	;45
+	dc.b $39	;46
+	dc.b $42	;47
+	dc.b $3C	;48
+	dc.b $61	;49
+	dc.b $5E	;4A
+	dc.b $5F	;4B
+	dc.b $60	;4C
+	dc.b $5D	;4D
+	dc.b $62	;4E
+	dc.b $64	;4F
+	dc.b $67	;50
+	dc.b $6E	;51
+	dc.b $75	;52
+	dc.b $66	;53
+	dc.b $68	;54
+	dc.b $6D	;55
+	dc.b $6F	;56
+	dc.b $70	;57
+	dc.b $77	;58
+	dc.b $78	;59
+	dc.b $63	;5A
+	dc.b $65	;5B
+	dc.b $69	;5C
+	dc.b $6A	;5D
+	dc.b $6B	;5E
+	dc.b $71	;5F
+	dc.b   8	;60
+	dc.b $7C	;61
+	dc.b $7D	;62
+	dc.b $7A	;63
+	dc.b $7B	;64
+	dc.b $79	;65
+	dc.b $72	;66
+	dc.b $73	;67
+	dc.b $76	;68
+	dc.b   9	;69
+	dc.b $FF	;6A
+	dc.b $FF
---
+++ split.py
@@ -0,0 +1,298 @@
+import math
+import os
+
+f = open("kid.bin", "rb")
+b = f.read()
+
+# read /length/ bytes starting at /addr/ as integer 
+btoi = lambda addr,length: int.from_bytes(b[addr:addr+length], byteorder='big')
+
+
+# get a single bit from position x
+def get_bit(x):
+    byte = x // 8
+    bitpos = x % 8
+    return (b[byte] >> (7-bitpos)) & 0x01
+
+
+# get a n bits starting from position x
+# first bit is most significant, last bit least significant
+def get_bits(x, n):
+    value = 0
+    for i in range(n):
+        value <<= 1
+        value += get_bit(x+i)
+    return value
+
+
+def length_enemy(x):
+    '''
+    (header)
+    32-bit address pointing to 'H1'
+    12 unknown bytes
+
+    H1 - unknown (byte)
+    H2 - Number of objects in level (byte)
+
+    H2 entries of 8 bytes each: 
+    AA - Object
+    BB - Flags (00 to respawn DRAGON after death, FF to make DRAGON kill permanent)
+    CCDD - Hit points + 1 (0000 - 7FFE)
+    EEFF - X position
+    GGHH - Y position
+    '''
+    data_start = btoi(x,4)
+    if data_start != x+16:
+        print("Gap between enemy header and data: {:x}!".format(x))
+    num_objects = btoi(data_start+1, 1)
+    return num_objects*8 + 2 + 16
+
+
+def length_tile(x):
+    '''
+    Data consists of two parts: a bitstream of control (key) bits,
+    and a byte stream of input bytes. The control bits determine
+    how to read and interpret the bytes.
+
+    2 bytes are the offset to the input bytes, followed by 
+    block of control bits and
+    block of input bytes
+
+    Control bits 0100011 with input 00 00 terminates compression.
+    '''
+    off_input = btoi(x,2)
+    input_start = x + off_input + 2
+    addr_input = input_start
+    bitpos = 8*(x+2)
+
+    def dprint(x, end):
+        pass
+        # if bitpos >= 8*(input_start-2):
+        #     print(x, end=end)
+
+    while bitpos < 8*input_start:
+        b0 = get_bit(bitpos)
+        bitpos += 1
+        dprint(b0, end=' ')
+        if b0 == 0:
+            b1 = get_bit(bitpos)
+            bitpos += 1
+            dprint(b1, end=' ')
+            if b1 == 0:
+                # one bit of extra info
+                bitpos += 1
+                # 00 reads one input byte
+                dprint("{:02x}".format(btoi(addr_input,1)), end='\n')
+                addr_input += 1
+            else:
+                # 01
+                b234 = get_bits(bitpos,3)
+                bitpos += 3
+                b56 = get_bits(bitpos,2)
+                bitpos += 2
+                if b56 == 3:
+                    # reads two input bytes if 01:xxx:11
+                    dprint("{:04x}".format(btoi(addr_input,2)), end='\n')
+                    addr_input += 2
+                    if btoi(addr_input-2,2) == 0 and b234 == 0:
+                        # bits are 0100011 and input is 2 bytes of 0 --> stop
+                        break
+                else:
+                    # reads one input byte if 01:xxx:xx
+                    dprint("{:02x}".format(btoi(addr_input,1)), end='\n')
+                    addr_input += 1
+        else:
+            # 1 reads one input byte
+            dprint("{:02x}".format(btoi(addr_input,1)), end='\n')
+            addr_input += 1
+
+    return addr_input - x
+
+
+def length_block(x):
+    bitpos = 8*x + 8 # we're skipping the first byte because it's $40
+    xbits = get_bits(bitpos, 4)
+    bitpos += 4
+    ybits = get_bits(bitpos, 4)
+    bitpos += 4
+
+    bitpos += 1 # hidden bit, we don't need this
+    block_type = get_bits(bitpos, 5)
+    bitpos += 5
+    #print(block_type)
+
+    while block_type != 0x1F:
+        x_pos = get_bits(bitpos, xbits)
+        bitpos += xbits
+        while x_pos != (1<<xbits) - 1:
+            #y_pos = get_bits(bitpos, ybits)
+            bitpos += ybits # we don't need the y position
+            #print(x_pos, y_pos)
+            if block_type == 1: # Prize
+                numblocks = get_bits(bitpos, 4)
+                bitpos += 4 + 1 + (numblocks+1) * 5
+            elif block_type == 3: # Ghost
+                bitpos += 31
+            elif block_type == 4: # Telepad
+                bitpos += 25
+            elif block_type == 10 or block_type == 12: # Drill
+                numblocks = get_bits(bitpos, 4)
+                bitpos += 4 + 1 + (numblocks+1) * 4
+            elif block_type == 11: # Lift (R)
+                pass
+            elif block_type == 16: # Collision Mod
+                numblocks = get_bits(bitpos, 4)
+                bitpos += 4 + 1 + (numblocks+1) * 3
+            else:
+                bitpos += 5
+
+            x_pos = get_bits(bitpos, xbits)
+            bitpos += xbits
+
+        bitpos += 1 # hidden bit, we don't need this
+        block_type = get_bits(bitpos, 5)
+        bitpos += 5
+        #print(block_type)
+
+    return math.ceil(bitpos/8) - x
+
+
+def length_mapeni(x):
+    # see: https://segaretro.org/Enigma_compression
+    inline_copy_value = btoi(x, 1)
+    render_flags_bits = 0
+    # only count how many bits of the second byte are set.
+    for i in range(5):
+        if get_bit(8*x+8+3+i):
+            render_flags_bits += 1
+
+    bitpos = 8*(x+6)
+    while True:
+        b = get_bit(bitpos)
+        if b == 0:
+            # two type bits and 4 repeat bits
+            bitpos += 2 + 4
+        else:
+            # reading the first bit again to get the full type
+            type_bits = get_bits(bitpos, 3)
+            bitpos += 3
+            repeat_bits = get_bits(bitpos, 4)
+            bitpos += 4
+            if type_bits == 7:
+                if repeat_bits == 0x0F:
+                    break # type is 11 and repeat bits 1111 -> DONE!
+                else:
+                    bitpos += (repeat_bits + 1) * (render_flags_bits + inline_copy_value)
+            else:
+                bitpos += render_flags_bits + inline_copy_value
+
+    return math.ceil(bitpos/8) - x
+
+
+def length_platform(x):
+    pos = x
+    x_pos = btoi(pos, 2)
+    while x_pos != 0xFFFF:
+        pos += 12
+        x_pos = btoi(pos, 2)
+    return pos+2 - x
+
+
+MapHeader_Index = 0x40342
+MapHeader_Offset = 0x4033A
+Platform_Index = 0x43A6
+Platform_Offset = 0x2BB6
+Number_Levels = 126
+
+os.makedirs("level", exist_ok=True)
+os.makedirs("level/block", exist_ok=True)
+os.makedirs("level/enemy", exist_ok=True)
+os.makedirs("level/platform", exist_ok=True)
+os.makedirs("level/foreground", exist_ok=True)
+os.makedirs("level/background", exist_ok=True)
+
+
+mapheaders = set()
+platform_addrs = dict()
+enemy_addrs = dict()
+foreground_addrs = dict()
+background_addrs = dict()
+block_addrs = dict()
+
+
+for lev in range(Number_Levels):
+    addr = btoi(MapHeader_Index+2*lev, 2) + MapHeader_Offset
+    platform = btoi(Platform_Index+2*lev, 2) + Platform_Offset
+    # need this because of load of duplicates
+    if platform not in platform_addrs:
+        l = length_platform(platform)
+        platform_addrs[platform] = lev
+        with open("level/platform/{:02X}.bin".format(lev), "wb") as out:
+            out.write(b[platform:platform+l])
+
+
+    if addr == 0x40FF6:
+        # for some entries, the pointer points to invalid data
+        # specifically, to the address after the last valid map header
+        continue
+
+    # don't process duplicate entries. They give us false overlap alarms.
+    if addr in mapheaders:
+        continue
+
+    mapheaders.add(addr)
+
+    '''
+    map header format:
+        dc.b    xsize, ysize, fgstyle, bgstyle
+        dc.w    playerx, playery, flagx, flagy
+        dc.l    foreground, block, background, enemy
+    '''
+    foreground = btoi(addr+12, 4)
+    block = btoi(addr+16, 4)
+    background = btoi(addr+20, 4)
+    enemy = btoi(addr+24, 4)
+
+    if enemy not in enemy_addrs:
+        enemy_addrs[enemy] = lev
+        l = length_enemy(enemy)
+        with open("level/enemy/{:02X}.bin".format(lev), "wb") as out:
+            out.write(b[enemy+4:enemy+l])
+
+    if foreground not in foreground_addrs:
+        foreground_addrs[foreground] = lev
+        l = length_tile(foreground)
+        with open("level/foreground/{:02X}.bin".format(lev), "wb") as out:
+            out.write(b[foreground:foreground+l])
+
+    if block not in block_addrs:
+        block_addrs[block] = lev
+        l = length_block(block)
+        with open("level/block/{:02X}.bin".format(lev), "wb") as out:
+            out.write(b[block:block+l])
+
+    if background not in background_addrs and background < 0x6F992:
+        btype = btoi(background, 1)
+        if btype == 0x80:
+            w1 = btoi(background, 2)
+            l2 = btoi(background+2, 4)
+            ref_addr = btoi(background+6, 4)
+            background_addrs[background] = (w1, l2, ref_addr)
+        else:
+            l = length_mapeni(background)
+            l = l + (l%2) # round up if length is odd.
+            background_addrs[background] = (lev,)
+            with open("level/background/{:02X}.bin".format(lev), "wb") as out:
+                out.write(b[background:background+l])            
+
+to_split = open("tools/to_split.txt")
+for line in to_split:
+    if line[0] == "#":
+        # hash symbol to remark a comment line
+        continue
+    entries = line.split()
+    start = int(entries[0], 16)
+    end = int(entries[1], 16)
+    fname = entries[2]
+    with open(fname, "wb") as out:
+        out.write(b[start:end])
